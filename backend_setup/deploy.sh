b9997c0d5f9f5d2a948addcd7e6a4a2a6dc1e159
Andrew Hinh <ajhinh@gmail.com>
2022-11-01 00:22:37 -0700

    Fixed continuous deployment script

--- backend_setup/deploy.sh
+++ backend_setup/deploy.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-
-UPSTREAM=${1:-'@{u}'}
-LOCAL=$(git rev-parse @)
-BASE=$(git merge-base @ "$UPSTREAM")
-
 while :
 do
-    git remote update
-    if [ "$LOCAL" = "$BASE" ]; then
+    git fetch
+    if [ "$(git rev-list HEAD...origin/main --count)" != 0 ]; then
         echo "Updating backend"
+        git pull
         jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=None --execute ./backend_setup/continuous_deployment.ipynb
     fi
 done
